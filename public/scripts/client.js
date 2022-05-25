58d6273b735bc45901d83b029e153d159d554618
Winston Limanto <wmlimanto@gmail.com>
2022-05-25 16:23:34 -0700

    add loadTweets that uses AJAX GET req & jQuery to fetch tweet data from server

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,33 +5,8 @@
  */
 
 $(document).ready(() => {
-  // Fake data taken from initial-tweets.json
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd"
-      },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ]
 
+  // create new tweet html using tweetData obj
   const createTweetElement = function (tweetData) {
     let $tweet = $(`
     <article class="tweets">
@@ -60,10 +35,9 @@ $(document).ready(() => {
     return $tweet;
   }
 
+  // loop over tweets, pass each tweet through createTweetElement and appends it to the tweets container
   const renderTweets = function (tweets) {
-    // loops through tweets
-    // calls createTweetElement for each tweet
-    // takes return value and appends it to the tweets container
+
     for (let tweet of tweets) {
       const $tweet = createTweetElement(tweet);
       $(".feed").append($tweet);
@@ -72,6 +46,7 @@ $(document).ready(() => {
 
   renderTweets(data);
 
+  // event listener on 'submit' that uses AJAX to POST (send) form data to server (/tweets)
   $("form").on("submit", function(event) {
     // prevent page refresh
     event.preventDefault();
@@ -84,6 +59,18 @@ $(document).ready(() => {
       data: data
     })
   })
-  
+
+  // use AJAX to GET (fetch) json data from server (/tweets)
+  const loadTweets = function () {
+    $.ajax({
+      method: "GET",
+      url: "/tweets",
+      dataType: "json"
+    })
+    .then(function (data) {
+      renderTweets(data);
+    })
+  };
+  loadTweets();
 })
 
